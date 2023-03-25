4fba702ff3792a4f36117f1c19327863c434303e
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-03-25 14:09:09 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /var/www/flaskapp
+cd /var/www/myapp
+
+# Install Python dependencies
 sudo apt-get update
-sudo apt-get install -y python3-pip
-sudo pip3 install virtualenv
-virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
+sudo apt-get install -y python3 python3-pip
+sudo pip3 install --upgrade pip
+sudo pip3 install -r requirements.txt
